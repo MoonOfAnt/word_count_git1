7726ab7fa196ad7957a27ceb0843c3b6aa51f7e1
BigAnt <1049205538@qq.com>
2020-02-22 16:37:12 +0800

    将项目文件copy到这个GitHub项目里

---
+++ wordcount/function.py
@@ -0,0 +1,22 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    user_text = request.GET['text']
+    num_count = len(user_text)
+
+    word_dict = {}
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    wort_dict_sorted = sorted(word_dict.items(), key = lambda w:w[1], reverse = True)
+
+    return render(request, 'count.html',{'count': num_count, 'text': user_text, 'word_dict': wort_dict_sorted})
+
+def about(request):
+    return render(request, 'about.html')
